384e2f9ac86b602d8079c0fadd8d021896bbf0e2
Sam Lightbody <samuel.lightbody@onxmaps.com>
2020-08-31 09:50:11 -0600

    more solutions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -3,19 +3,19 @@
 // You can read more about it at https://doc.rust-lang.org/std/convert/trait.From.html
 #[derive(Debug)]
 struct Person {
-    name: String,
-    age: usize,
+  name: String,
+  age: usize,
 }
 
 // We implement the Default trait to use it as a fallback
 // when the provided string is not convertible into a Person object
 impl Default for Person {
-    fn default() -> Person {
-        Person {
-            name: String::from("John"),
-            age: 30,
-        }
+  fn default() -> Person {
+    Person {
+      name: String::from("John"),
+      age: 30,
     }
+  }
 }
 
 // I AM NOT DONE
@@ -33,48 +33,58 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person onject with the results
 impl From<&str> for Person {
-    fn from(s: &str) -> Person {
+  fn from(s: &str) -> Person {
+    match s {
+      "" => Person::default(),
+      "_" => {
+        let parsed: Vec<&str> = s.split(",").collect();
+        let mut new_guy = Person::default();
+        new_guy.name = parsed[0].to_string();
+        new_guy.age = parsed[1].to_string().parse()::<i32>.unwrap() as usize;
+        new_guy
+      }
     }
+  }
 }
 
 fn main() {
-    // Use the `from` function
-    let p1 = Person::from("Mark,20");
-    // Since From is implemented for Person, we should be able to use Into
-    let p2: Person = "Gerald,70".into();
-    println!("{:?}", p1);
-    println!("{:?}", p2);
+  // Use the `from` function
+  let p1 = Person::from("Mark,20");
+  // Since From is implemented for Person, we should be able to use Into
+  let p2: Person = "Gerald,70".into();
+  println!("{:?}", p1);
+  println!("{:?}", p2);
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    #[test]
-    fn test_default() {
-        // Test that the default person is 30 year old John
-        let dp = Person::default();
-        assert_eq!(dp.name, "John");
-        assert_eq!(dp.age, 30);
-    }
-    #[test]
-    fn test_bad_convert() {
-        // Test that John is returned when bad string is provided
-        let p = Person::from("");
-        assert_eq!(p.name, "John");
-        assert_eq!(p.age, 30);
-    }
-    #[test]
-    fn test_good_convert() {
-        // Test that "Mark,20" works
-        let p = Person::from("Mark,20");
-        assert_eq!(p.name, "Mark");
-        assert_eq!(p.age, 20);
-    }
-    #[test]
-    fn test_bad_age() {
-        // Test that "Mark.twenty" will return the default person due to an error in parsing age
-        let p = Person::from("Mark,twenty");
-        assert_eq!(p.name, "John");
-        assert_eq!(p.age, 30);
-    }
+  use super::*;
+  #[test]
+  fn test_default() {
+    // Test that the default person is 30 year old John
+    let dp = Person::default();
+    assert_eq!(dp.name, "John");
+    assert_eq!(dp.age, 30);
+  }
+  #[test]
+  fn test_bad_convert() {
+    // Test that John is returned when bad string is provided
+    let p = Person::from("");
+    assert_eq!(p.name, "John");
+    assert_eq!(p.age, 30);
+  }
+  #[test]
+  fn test_good_convert() {
+    // Test that "Mark,20" works
+    let p = Person::from("Mark,20");
+    assert_eq!(p.name, "Mark");
+    assert_eq!(p.age, 20);
+  }
+  #[test]
+  fn test_bad_age() {
+    // Test that "Mark.twenty" will return the default person due to an error in parsing age
+    let p = Person::from("Mark,twenty");
+    assert_eq!(p.name, "John");
+    assert_eq!(p.age, 30);
+  }
 }
--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -1,35 +1,35 @@
 // traits2.rs
-// 
+//
 // Your task is to implement the trait
 // `AppendBar' for a vector of strings.
-// 
+//
 // To implement this trait, consider for
 // a moment what it means to 'append "Bar"'
 // to a vector of strings.
-// 
+//
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
-
 trait AppendBar {
-    fn append_bar(self) -> Self;
+  fn append_bar(self) -> Self;
 }
 
-//TODO: Add your code here
-
-
-
+impl AppendBar for Vec<String> {
+  //Add your code here
+  fn append_bar(mut self) -> Self {
+    self.push("Bar".to_string());
+    self
+  }
+}
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn is_vec_pop_eq_bar() {
-        let mut foo = vec![String::from("Foo")].append_bar();
-        assert_eq!(foo.pop().unwrap(), String::from("Bar"));
-        assert_eq!(foo.pop().unwrap(), String::from("Foo"));
-    }
-
+  #[test]
+  fn is_vec_pop_eq_bar() {
+    let mut foo = vec![String::from("Foo")].append_bar();
+    assert_eq!(foo.pop().unwrap(), String::from("Bar"));
+    assert_eq!(foo.pop().unwrap(), String::from("Foo"));
+  }
 }
